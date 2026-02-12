d18e5106f379586dba0aaf733f0ae3eee4163121
Ironowl1907 <facuguialtana7@gmail.com>
2026-02-12 21:51:27 +0000

    Setting up boilerplate

--- src/parser.c
+++ src/parser.c
@@ -8,6 +8,12 @@
 static token_t consume(parser_t *ctx);
 static token_t peek(parser_t *ctx);
 
+static node_id parse_identifier(parser_t *ctx);
+static node_id parse_litteral(parser_t *ctx);
+static node_id parse_return(parser_t *ctx);
+static node_id parse_if(parser_t *ctx);
+static node_id parse_function_declaration(parser_t *ctx);
+
 parser_t *parser_create(ast_t *ast, lexer_t *lexer) {
   assert(ast);
   assert(lexer);
@@ -39,6 +45,17 @@ static token_t consume(parser_t *ctx) {
 static token_t peek(parser_t *ctx) {
   assert(ctx);
   return lexer_get_token(ctx->lexer, ctx->cursor);
+}
+
+static node_id parse_function_declaration(parser_t *ctx) {
+
+}
+
+static node_id parse_identifier(parser_t *ctx) {
+  return ast_create_node(ctx->ast,
+                         (node_t){.type = NODE_TYPE_IDENTIFIER,
+                                  .as.identifier.type = TYPE_INT,
+                                  .as.identifier.name = consume(ctx).data});
 }
 
 int parser_parse(void) {}
