d10fe3330e97f77243155fd64bdab9e2bd0b7823
Ironowl1907 <facuguialtana7@gmail.com>
2026-03-05 00:37:58 +0000

    Redoing codegen system, working on new api

--- include/codegen.h
+++ include/codegen.h
@@ -1,43 +1,36 @@
 #ifndef CODEGEN_H
 #define CODEGEN_H
 
-#include <stddef.h>
+#include "ast.h"
+#include <llvm-c/Types.h>
+#include <stdint.h>
+#define SYMBOL_TABLE_MAX 256;
 
-#include <llvm-c/Core.h>
-#include <llvm-c/TargetMachine.h>
+typedef struct {
+  const char *name;
+  size_t name_size;
+  LLVMValueRef slot;
+} symbol_entry_t;
 
-typedef struct codegen_context {
+typedef struct {
+  symbol_entry_t entries[SYMBOL_TABLE_MAX];
+  uint32_t size;
+} symbol_table_t;
+
+typedef struct {
+  ast_t ast;
+
   LLVMContextRef context;
   LLVMModuleRef module;
   LLVMBuilderRef builder;
 
-  LLVMTargetMachineRef target_machine;
+  symbol_table_t symbols;
 } codegen_context_t;
 
-/*
- * Error handling
- */
-typedef enum {
-  CODEGEN_OK = 0,
-  CODEGEN_ERROR_NULL_ARGUMENT,
-  CODEGEN_ERROR_LLVM_INIT_FAILED,
-  CODEGEN_ERROR_MODULE_VERIFICATION_FAILED,
-  CODEGEN_ERROR_TARGET_INIT_FAILED,
-  CODEGEN_ERROR_EMIT_FAILED
-} codegen_error_t;
-
-codegen_context_t *codegen_create(const char *module_name,
-                                  codegen_error_t *err);
-
-void codegen_destroy(codegen_context_t *ctx);
-
-LLVMContextRef codegen_get_llvm_context(codegen_context_t *ctx);
-
-LLVMModuleRef codegen_get_module(codegen_context_t *ctx);
-
-LLVMBuilderRef codegen_get_builder(codegen_context_t *ctx);
-
-int codegen_verify_module(codegen_context_t *ctx);
-
+////////////////////////////////////////////////////////
+//////////////////// Life Cycle /////////////////////////
+////////////////////////////////////////////////////////
+codegen_context_t *codegen_ctx_create(ast_t *ast, const char *module_name);
+void codegen_ctx_destroy(codegen_context_t *ctx);
 
 #endif
